07039c32b8b2cd28c6fb09d1e218a872f0444884
elloveronika <ello.veronika92@gmail.com>
2021-04-06 23:30:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+class Calculator{
+    constructor()
+}
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-equals]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+const deleteButton = document.querySelectorAll('[data-delete]')
+const allClearButton = document.querySelectorAAll('[data-all-clear]')
+const previousOperand = document.querySelectorAll('[data-previous-operand]')
+const currentOperand = document.querySelectorAll('[data-current-operand]')
+
+
+
